35869de054294496b0d693367e28006d75059242
dev-yhc <hyunchul.yang@gmail.com>
2022-05-29 16:00:37 +0900

    git init

--- src/main/java/com/devAlone/springboot/Application.java
+++ src/main/java/com/devAlone/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing -> JpaConfig에서 관리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
