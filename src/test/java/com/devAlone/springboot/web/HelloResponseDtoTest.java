70f1a8ec830897dded9b49212b669e0aa2e7f7c5
dev-yhc <hyunchul.yang@gmail.com>
2022-06-11 18:05:39 +0900

    update to java11, springboot 2.6.8

--- src/test/java/com/devAlone/springboot/web/HelloResponseDtoTest.java
+++ src/test/java/com/devAlone/springboot/web/HelloResponseDtoTest.java
@@ -1,7 +1,7 @@
 package com.devAlone.springboot.web;
 
 import com.devAlone.springboot.web.dto.HelloResponseDto;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
